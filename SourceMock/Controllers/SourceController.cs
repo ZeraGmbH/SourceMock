d20bdae9b5993807afe8243f7757f2083f54e508
Florian Becker <fbe@zerycon.de>
2023-05-31 14:57:39 +0200

    Added basic loadpoint

---
+++ SourceMock/Controllers/SourceController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SourceMock.Model;
+
+namespace SourceMock.Controllers
+{
+
+    [ApiVersion("1.0")]
+    [ApiController]
+    [Route("api/v{version:apiVersion}/[controller]")]
+    public class SourceController : Controller
+    {
+        [HttpPost("Set")]
+        public IActionResult SetValues([FromBody] Loadpoint loadpoint)
+        {
+            return Ok();
+        }
+
+        [HttpPost("TurnOn")]
+        public IActionResult TurnOn()
+        {
+            return Ok();
+        }
+
+        [HttpPost("TurnOff")]
+        public IActionResult TurnOff()
+        {
+            return Ok();
+        }
+    }
+}
---
+++ SourceMock/Model/Loadpoint.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SourceMock.Model
+{
+    public class Loadpoint
+    {
+        private const Double MINIMUM_ANGLE = 0;
+        private const Double MAXIMUM_ANGLE = 360;
+
+        [Range(MINIMUM_ANGLE, MAXIMUM_ANGLE)]
+        public Double PhaseAngleVoltage { get; set; }
+    }
+}

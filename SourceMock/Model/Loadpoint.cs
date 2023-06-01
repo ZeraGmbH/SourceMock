6862d36ef5b080130b19cc8e7d680b49414ccb50
Florian Becker <fbe@zerycon.de>
2023-06-01 19:31:47 +0200

    Added documentation to Loadpoint class

--- SourceMock/Model/Loadpoint.cs
+++ SourceMock/Model/Loadpoint.cs
@@ -3,17 +3,39 @@ using System.ComponentModel.DataAnnotations;
 
 namespace SourceMock.Model
 {
+    /// <summary>
+    /// Describes a specific loadpoint.
+    /// </summary>
+    [Serializable]
     public class Loadpoint
     {
         private const double MINIMUM_ANGLE = 0;
         private const double MAXIMUM_ANGLE = 360;
 
+        /// <summary>
+        /// A list of voltages for the different phases in the order L1, L2, L3.
+        /// </summary>
         public IEnumerable<double> Voltages { get; set; } = new List<double>();
+
+        /// <summary>
+        /// A list of currents for the differnt phases in the order L1, L2, L3.
+        /// </summary>
         public IEnumerable<double> Currents { get; set; } = new List<double>();
 
+        /// <summary>
+        /// The frequency.
+        /// </summary>
+        public double Frequency { get; set; }
+
+        /// <summary>
+        /// The angles of the voltages in the differnt phases in the order L1, L2, L3.
+        /// </summary>
         [RangeEnumerable(MINIMUM_ANGLE, MAXIMUM_ANGLE)]
         public IEnumerable<double> PhaseAnglesVoltage { get; set; } = new List<double>();
 
+        /// <summary>
+        /// The angle of the currents in the differnt phases in the order L1, L2, L3.
+        /// </summary>
         [RangeEnumerable(MINIMUM_ANGLE, MAXIMUM_ANGLE)]
         public IEnumerable<double> PhaseAnglesCurrent { get; set; } = new List<double>();
     }

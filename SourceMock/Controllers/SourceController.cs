ffd7b0bddd3217f4683bf7ca1ab170934960f210
Florian Becker <fbe@zerycon.de>
2023-06-09 11:44:25 +0200

    Added SimulatedSource's capabilities to rest API

--- SourceMock/Controllers/SourceController.cs
+++ SourceMock/Controllers/SourceController.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 
 using SourceMock.Actions.LoadpointValidator;
+using SourceMock.Actions.Source;
 using SourceMock.Model;
 
 namespace SourceMock.Controllers
@@ -14,13 +15,17 @@ namespace SourceMock.Controllers
     public class SourceController : Controller
     {
         private ILogger logger;
+        private ISource source;
+
         /// <summary>
         /// Constructor for a SouceController.
         /// </summary>
         /// <param name="logger">Injected logger.</param>
-        public SourceController(ILogger<SourceController> logger)
+        /// <param name="source">Injected source.</param>
+        public SourceController(ILogger<SourceController> logger, ISource source)
         {
             this.logger = logger;
+            this.source = source;
         }
 
         /// <summary>
@@ -31,11 +36,13 @@ namespace SourceMock.Controllers
         /// <response code="200">If the loadpoint could be set correctly.</response>
         /// <response code="400">If the loadpoint was malformed.</response>
         /// <response code="422">If the loadpoint was wellformed but invalid.</response>
-        [HttpPost("Set")]
+        /// <response code="500">If an unexpected error occured.</response>
+        [HttpPost("SetLoadpoint")]
         [ProducesResponseType(StatusCodes.Status200OK)]
         [ProducesResponseType(StatusCodes.Status400BadRequest)]
         [ProducesResponseType(StatusCodes.Status422UnprocessableEntity)]
-        public ActionResult SetValues([FromBody] Loadpoint loadpoint)
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        public ActionResult SetLoadpoint([FromBody] Loadpoint loadpoint)
         {
             var validationResult = LoadpointValidator.Validate(loadpoint);
             if (validationResult != LoadpointValidator.ValidationResult.OK)
@@ -44,7 +51,19 @@ namespace SourceMock.Controllers
                 return Problem(detail: validationResult.ToString(), statusCode: StatusCodes.Status422UnprocessableEntity);
             }
 
-            return Ok();
+            switch (source.SetLoadpoint(loadpoint))
+            {
+                case SourceResult.SUCCESS:
+                    return Ok();
+                case SourceResult.LOADPOINT_NOT_SUITABLE_DIFFERENT_NUMBER_OF_PHASES:
+                    return Problem(
+                        detail: SourceResult.LOADPOINT_NOT_SUITABLE_DIFFERENT_NUMBER_OF_PHASES.ToString(),
+                        statusCode: StatusCodes.Status422UnprocessableEntity);
+                default:
+                    return Problem(
+                        detail: "Unkown Response from source.",
+                        statusCode: StatusCodes.Status500InternalServerError);
+            }
         }
 
         /// <summary>
@@ -54,13 +73,27 @@ namespace SourceMock.Controllers
         /// <response code="200">If the loadpoint was set successfully.</response>
         /// <response code="409">If the loadpoint could not be set successfully.</response>
         /// <response code="424">If no loadpoint is currently set.</response>
+        /// <response code="500">If an unexpected error occured.</response>
         [HttpPost("TurnOn")]
         [ProducesResponseType(StatusCodes.Status200OK)]
         [ProducesResponseType(StatusCodes.Status409Conflict)]
         [ProducesResponseType(StatusCodes.Status424FailedDependency)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
         public ActionResult TurnOn()
         {
-            return Ok();
+            switch (source.TurnOn())
+            {
+                case SourceResult.SUCCESS:
+                    return Ok();
+                case SourceResult.NO_LOADPOINT_SET:
+                    return Problem(
+                        detail: SourceResult.NO_LOADPOINT_SET.ToString(),
+                        statusCode: StatusCodes.Status424FailedDependency);
+                default:
+                    return Problem(
+                        detail: "Unkown Response from source.",
+                        statusCode: StatusCodes.Status500InternalServerError);
+            }
         }
 
         /// <summary>
@@ -69,12 +102,22 @@ namespace SourceMock.Controllers
         /// <returns>The result of the operation, see responses.</returns>
         /// <response code="200">If the source was turned off successfully.</response>
         /// <response code="409">If the source could not be turned off successfully.</response>
+        /// <response code="500">If an unexpected error occured.</response>
         [HttpPost("TurnOff")]
         [ProducesResponseType(StatusCodes.Status200OK)]
         [ProducesResponseType(StatusCodes.Status409Conflict)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
         public ActionResult TurnOff()
         {
-            return Ok();
+            switch (source.TurnOff())
+            {
+                case SourceResult.SUCCESS:
+                    return Ok();
+                default:
+                    return Problem(
+                        detail: "Unkown Response from source.",
+                        statusCode: StatusCodes.Status500InternalServerError);
+            }
         }
     }
 }

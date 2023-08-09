f9d976ce421b4ae1cbc893f60983eb00bc2ada11
mdoerr2s <mika.doerr@smail.inf.h-brs.de>
2023-08-09 14:59:23 +0200

    Introduce VeinClient
    - Getter for test data (System Entity Name)
    - Setter for test data (Customer City)
    - Added tests for getter/setter
    
    Signed-off-by: mdoerr2s <mika.doerr@smail.inf.h-brs.de>

--- SourceMock.Tests/Actions/VeinSource/VeinSourceTests.cs
+++ SourceMock.Tests/Actions/VeinSource/VeinSourceTests.cs
@@ -1,46 +1,48 @@
 using System.Net;
-using System.Net.Http.Headers;
 
-using Newtonsoft.Json.Linq;
+using SourceMock.Actions.VeinSource;
 
 namespace SourceMock.Tests.Actions.VeinSource
 {
     internal class VeinSourceTests
     {
-        const string hostIp = "127.0.0.1";
-        const int hostPort = 8080;
+        const string HOST_IP = "127.0.0.1";
+        const int HOST_PORT = 8080;
         #region PositiveTestCases
         [Test]
-        public async Task MustConnectToVeinAPI()
+        public void GetSystemEntityNameFromVein()
         {
 #pragma warning disable CS0162
             if (!IntegrationTestSwitches.DO_VEIN_TESTS) Assert.Ignore();
 #pragma warning restore CS0162
             // Arrange
             HttpClient client = new();
-            client.DefaultRequestHeaders.Accept.Clear();
-            client.DefaultRequestHeaders.Accept.Add(
-                new MediaTypeWithQualityHeaderValue("application/json"));
-            client.DefaultRequestHeaders.Add("User-Agent", ".NET Foundation Repository Reporter");
-            string payload = @"{ 
-                                ""EntityID"": 0,
-                                ""componentName"": ""EntityName""
-                                }";
-            var stringContent = new StringContent(payload);
+            VeinClient veinClient = new(client, HOST_IP, HOST_PORT);
 
             // Act
-            var response = await client.PostAsync($"http://{hostIp}:{hostPort}/api/v1/Vein/GetInfo", stringContent);
-
-            var responseContent = JObject.Parse(await response.Content.ReadAsStringAsync());
-            var status = responseContent["status"].ToString();
-            var componentValue = responseContent["ReturnInformation"].ToString();
+            var result = veinClient.GetSystemEntityName();
 
             // Assert
-            Assert.AreEqual(HttpStatusCode.OK, response.StatusCode);
-            Assert.AreEqual("200", status);
-            Assert.AreEqual("_System", componentValue);
+            Assert.AreEqual(HttpStatusCode.OK, result.Status);
+            Assert.AreEqual("_System", result.Value);
         }
 
+        [Test]
+        public void SetCustomerCityToVein()
+        {
+#pragma warning disable CS0162
+            if (!IntegrationTestSwitches.DO_VEIN_TESTS) Assert.Ignore();
+#pragma warning restore CS0162
+            // Arrange
+            HttpClient client = new();
+            VeinClient veinClient = new(client, HOST_IP, HOST_PORT);
+
+            // Act
+
+            HttpStatusCode statusCode = veinClient.SetCustomerCity("foo");
+            // Assert
+            Assert.AreEqual(HttpStatusCode.OK, statusCode);
+        }
         #endregion
     }
 }
---
+++ SourceMock/Actions/VeinSource/VeinClient.cs
@@ -0,0 +1,104 @@
+using System.Net;
+using System.Net.Http.Headers;
+
+using Newtonsoft.Json.Linq;
+
+namespace SourceMock.Actions.VeinSource
+{
+    /// <summary>
+    /// Connects to VeinAPI to get and set components in Vein Entities
+    /// </summary>
+    public class VeinClient
+    {
+        /// <summary>
+        /// Creates an instance of VeinClient
+        /// </summary>
+        /// <param name="client">Injected HttpClient to be used by this instance</param>
+        /// <param name="hostIp">Ip which the Vein API listens to</param>
+        /// <param name="hostPort">Port on which the Vein API listens</param>
+        public VeinClient(HttpClient client, string hostIp, int hostPort)
+        {
+            _client = client;
+            _hostIp = hostIp;
+            _hostPort = hostPort;
+
+            _client.DefaultRequestHeaders.Accept.Clear();
+            _client.DefaultRequestHeaders.Accept.Add(
+                new MediaTypeWithQualityHeaderValue("application/json"));
+        }
+
+        /// <summary>
+        /// Query EntityName of Vein System Entity
+        /// </summary>
+        /// <returns>String of System Entity Name</returns>
+        public VeinGetResult<string> GetSystemEntityName()
+        {
+            JObject json = GetFromVein(EntityIds.SYSTEM, "EntityName");
+            VeinGetResult<string> result = new()
+            {
+                Status = (HttpStatusCode)Int32.Parse(json["status"]?.ToString() ?? ""),
+                Value = json["ReturnInformation"]?.ToString() ?? ""
+            };
+            return result;
+        }
+
+        /// <summary>
+        /// Set Customer City of Vein Customer Entity
+        /// </summary>
+        /// <param name="cityName">City Name of Customer</param>
+        /// <returns>HttpStatusCode of Request</returns>
+        public HttpStatusCode SetCustomerCity(string cityName)
+        {
+            JObject json = SetToVein(EntityIds.CUSTOMER_DATA, "PAR_CustomerCity", cityName);
+
+            return (HttpStatusCode)Int32.Parse(json["status"]?.ToString() ?? "");
+        }
+
+        private JObject GetFromVein(EntityIds entityId, string componentName)
+        {
+            string payload = $"{{\"EntityID\": {(int)entityId}, \"componentName\": \"{componentName}\"}}";
+            StringContent stringContent = new(payload);
+
+            HttpResponseMessage response = _client.PostAsync($"http://{_hostIp}:{_hostPort}/api/v1/Vein/GetInfo", stringContent).Result;
+
+            return JObject.Parse(response.Content.ReadAsStringAsync().Result);
+        }
+
+        private JObject SetToVein(EntityIds entityId, string componentName, string value)
+        {
+            string payload = $"{{\"EntityID\": {(int)entityId}, \"componentName\": \"{componentName}\", \"newValue\": \"{value}\"}}";
+            StringContent stringContent = new(payload);
+
+            HttpResponseMessage response = _client.PostAsync($"http://{_hostIp}:{_hostPort}/api/v1/Vein/SetInfo", stringContent).Result;
+
+            return JObject.Parse(response.Content.ReadAsStringAsync().Result);
+        }
+
+        private readonly HttpClient _client;
+        private readonly string _hostIp;
+        private readonly int _hostPort;
+
+        private enum EntityIds
+        {
+            SYSTEM = 0,
+            CUSTOMER_DATA = 200
+        }
+
+        /// <summary>
+        /// Result of GetInfo request to Vein API
+        /// </summary>
+        /// <typeparam name="T">Type of return value</typeparam>
+        public struct VeinGetResult<T>
+        {
+            /// <summary>
+            /// HttpStatusCode of GetInfo response
+            /// </summary>
+            public HttpStatusCode Status;
+            /// <summary>
+            /// Return value of GetInfo response
+            /// </summary>
+            public T Value;
+        }
+
+    }
+}

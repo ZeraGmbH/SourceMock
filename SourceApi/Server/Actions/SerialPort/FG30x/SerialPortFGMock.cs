0d73449edd8813d9d7c51e945a16620901befa7c
Dr. Jochen Manns <J.Manns@zera.de>
2023-12-08 08:49:06 +0100

    Mocking actual values with random number.

--- SourceApi/Server/Actions/SerialPort/FG30x/SerialPortFGMock.cs
+++ SourceApi/Server/Actions/SerialPort/FG30x/SerialPortFGMock.cs
@@ -37,16 +37,59 @@ public class SerialPortFGMock : ISerialPort
         return info.Reply;
     }
 
+    /// <summary>
+    /// Generate a random factor.
+    /// </summary>
+    /// <param name="number"></param>
+    /// <returns>Number scaled wit a random factor between -1% and +1%.</returns>
+    private static double MockNumber(double number) => number * (1.0 + Random.Shared.Next(-1000, +1000) / 100000.0);
+
     /// <inheritdoc/>
     public virtual void WriteLine(string command)
     {
         switch (command)
         {
             case "TS":
-                _replies.Enqueue("TSFG301   V385");
+                _replies.Enqueue("TSFG301   V299");
                 break;
             case "MI":
                 _replies.Enqueue("MI4LW;3LW;4LBK;4LBE;3LBK;3LBA;3BKB;3LBE;3LWR;4LBF;1PHT;1PHR;1PHA;4LS;3LS;4LQ6;3LQ6;4Q6K;3Q6K;4LSG;3LSG;4LBG;3LBG;");
+                break;
+            case "BU":
+                /* Range Voltage: 250V */
+                _replies.Enqueue("BU250.000");
+                break;
+            case "AU":
+                /* Voltage: 200V, 250V, 300V */
+                _replies.Enqueue($"AUR{MockNumber(16000):00000}S{MockNumber(20000):00000}T{MockNumber(24000):00000}");
+                break;
+            case "BI":
+                // Range Current: 5A */
+                _replies.Enqueue("BI5.000");
+                break;
+            case "AI":
+                /* Current: 2A, 1A, 3A */
+                _replies.Enqueue($"AIR{MockNumber(8000):00000}S{MockNumber(4000):00000}T{MockNumber(12000):00000}");
+                break;
+            case "AW":
+                /* Angle (V,A): (0, 5), (110, 130), (245, 231) */
+                _replies.Enqueue($"AWR{MockNumber(0):000.0}{MockNumber(5):000.0}S{MockNumber(110):000.0}{MockNumber(130):000.0}T{MockNumber(245):000.0}{MockNumber(231):000.0}");
+                break;
+            case "MP":
+                /* Active Power: 400W, 235W, 900W => 1535W */
+                _replies.Enqueue($"MPR{MockNumber(400):0.0};S{MockNumber(235):0.0};T{MockNumber(900):0.0}");
+                break;
+            case "MQ":
+                /* Reactive Power: 35var, 85var, -80var => 40var */
+                _replies.Enqueue($"MQR{MockNumber(35):0.0};S{MockNumber(85):0.0};T{MockNumber(-80):0.0}");
+                break;
+            case "MS":
+                /* Apparent power: 400VA, 250VA, 900VA => 1550VA */
+                _replies.Enqueue($"MSR{MockNumber(400):0.0};S{MockNumber(250):0.0};T{MockNumber(900):0.0}");
+                break;
+            case "AF":
+                /* Frequency: 50Hz */
+                _replies.Enqueue($"AF{MockNumber(50.0):00.00}");
                 break;
             default:
                 {

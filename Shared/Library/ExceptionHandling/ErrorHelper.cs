f56b45f9454184a71cfe3350ac503b7c0c3f6aca
Jurjut <t.jurjut@zera.de>
2023-12-14 15:35:29 +0100

    Helper method for module specific exception filters

---
+++ Shared/Library/ExceptionHandling/ErrorHelper.cs
@@ -0,0 +1,32 @@
+using Microsoft.AspNetCore.Mvc;
+using SharedLibrary.Models;
+
+namespace SharedLibrary.ExceptionHandling;
+
+/// <summary>
+/// ProblemDetails object
+/// </summary>
+public static class ErrorHelper
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    /// <param name="detail"></param>
+    /// <param name="status"></param>
+    /// <param name="samErrorCode"></param>
+    /// <param name="args"></param>
+    /// <returns></returns>
+    public static IActionResult CreateProblemDetails<T>(string detail, int status, T samErrorCode, params object[] args) where T : notnull
+    {
+        return new ObjectResult(new ProblemDetails
+        {
+            Detail = detail,
+            Status = status,
+            Extensions = {
+                { SamDetailExtensions.SamErrorCode.ToString(), samErrorCode.ToString() },
+                { SamDetailExtensions.SamErrorArgs.ToString(), args }
+             }
+        });
+    }
+}

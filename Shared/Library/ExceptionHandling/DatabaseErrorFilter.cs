ca0047b9b8e7b13c4599b00539f32d837fa99b86
Jurjut <t.jurjut@zera.de>
2023-12-15 10:45:08 +0100

    Cosmetics

--- Shared/Library/ExceptionHandling/DatabaseErrorFilter.cs
+++ Shared/Library/ExceptionHandling/DatabaseErrorFilter.cs
@@ -1,3 +1,4 @@
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc.Filters;
 using MongoDB.Driver;
 using SharedLibrary.Models;
@@ -19,13 +20,23 @@ public class DatabaseErrorFilter : IExceptionFilter
         var exception = context.Exception;
         if (exception is ItemNotFoundException itemNotFoundException)
         {
-            context.Result = ErrorHelper.CreateProblemDetails(exception.Message, status: 422, samErrorCode: SamDatabaseError.ITEM_NOT_FOUND, itemNotFoundException.ItemId);
+            context.Result = ErrorHelper.CreateProblemDetails(
+                exception.Message,
+                status: StatusCodes.Status422UnprocessableEntity,
+                samErrorCode: SamDatabaseError.ITEM_NOT_FOUND,
+                itemNotFoundException.ItemId
+                );
             context.ExceptionHandled = true;
             return;
         }
         if (exception is MongoException)
         {
-            context.Result = ErrorHelper.CreateProblemDetails(exception.Message, status: 400, samErrorCode: SamDatabaseError.DATABASE_ERROR, "");
+            context.Result = ErrorHelper.CreateProblemDetails(
+                exception.Message,
+                status: StatusCodes.Status400BadRequest,
+                samErrorCode: SamDatabaseError.DATABASE_ERROR,
+                ""
+                );
             context.ExceptionHandled = true;
         }
     }

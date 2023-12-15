7412e74efa11b263d4aeb67c4ab8008c4df3398c
Dr. Jochen Manns <J.Manns@zera.de>
2023-12-15 12:09:39 +0100

    Added ActionResultMapper tests.

---
+++ SerialPortProxy/Tests/ActionResultMapperTests.cs
@@ -0,0 +1,40 @@
+using System.Linq.Expressions;
+using Amazon.Runtime.Internal.Util;
+using Microsoft.AspNetCore.Mvc;
+using SerialPortProxy;
+using SharedLibrary.Models;
+
+namespace SerialPortProxyTests;
+
+[TestFixture]
+public class ActionResultMapperTests
+{
+    [TestCase(typeof(ArgumentException), SerialPortErrorCodes.SerialPortBadRequest)]
+    [TestCase(typeof(InvalidOperationException), SerialPortErrorCodes.SerialPortBadRequest)]
+    [TestCase(typeof(OperationCanceledException), SerialPortErrorCodes.SerialPortAborted)]
+    [TestCase(typeof(TimeoutException), SerialPortErrorCodes.SerialPortTimeOut)]
+    public async Task Will_Generate_Action_Result(Type exception, SerialPortErrorCodes expected)
+    {
+        var result = await ActionResultMapper.SafeExecuteSerialPortCommand(() => Task.FromException((Exception)Activator.CreateInstance(exception)!));
+
+        if (result is ObjectResult objectResult)
+            if (objectResult.Value is ProblemDetails problemDetails)
+            {
+                Assert.That(problemDetails.Extensions[SamDetailExtensions.SamErrorCode.ToString()], Is.EqualTo(expected.ToString()));
+
+                return;
+            }
+
+        Assert.Fail("no problem details");
+    }
+
+    [TestCase(typeof(NotImplementedException))]
+    [TestCase(typeof(NotSupportedException))]
+    [TestCase(typeof(ArgumentOutOfRangeException))]
+    public void Will_Passthough_Unknown_Exception(Type exception)
+    {
+        var error = Assert.CatchAsync(() => ActionResultMapper.SafeExecuteSerialPortCommand(() => Task.FromException((Exception)Activator.CreateInstance(exception)!)));
+
+        Assert.That(error, Is.TypeOf(exception));
+    }
+}

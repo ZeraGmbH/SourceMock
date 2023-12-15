faa2976259353661e02c9a12f616cf3140fe7314
Dr. Jochen Manns <J.Manns@zera.de>
2023-12-15 14:05:58 +0100

    Revert "Moved global exception filter to WebServer in WebSamDeviceApis"
    This reverts commit 8e55751417fb259f5f5ace8ce69c50c68fa9674b.

--- WebServer/Server/Program.cs
+++ WebServer/Server/Program.cs
@@ -5,7 +5,7 @@ using System.Globalization;
 using SerialPortProxy;
 using SharedLibrary;
 using SourceApi;
-using WebSamDeviceApis.ExceptionHandling;
+using SharedLibrary.ExceptionHandling;
 
 CultureInfo.CurrentCulture = CultureInfo.CurrentUICulture = CultureInfo.DefaultThreadCurrentCulture = CultureInfo.DefaultThreadCurrentUICulture = CultureInfo.GetCultureInfo("en-us");
 

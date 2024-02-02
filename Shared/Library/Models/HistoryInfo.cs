b7181db26867066241c697178161e3024d5c2de2
Dr. Jochen Manns <dev-2@psimarron.net>
2024-02-02 17:10:27 +0100

    Corrected history item model - all fields are required.

--- Shared/Library/Models/HistoryInfo.cs
+++ Shared/Library/Models/HistoryInfo.cs
@@ -1,3 +1,5 @@
+using System.ComponentModel.DataAnnotations;
+using System.Diagnostics.CodeAnalysis;
 using MongoDB.Bson.Serialization.Attributes;
 
 namespace SharedLibrary.Models;
@@ -10,31 +12,31 @@ public class HistoryInfo
     /// <summary>
     /// Exact time when the item was added to the database.
     /// </summary>
-    [BsonElement("createdAt")]
+    [BsonElement("createdAt"), Required, NotNull]
     public DateTime CreatedAt { get; set; }
 
     /// <summary>
     /// User who added the item to the database.
     /// </summary>
-    [BsonElement("createdBy")]
+    [BsonElement("createdBy"), Required, NotNull]
     public string CreatedBy { get; set; } = null!;
 
     /// <summary>
     /// Exact time when the item was last modified.
     /// </summary>
-    [BsonElement("modifiedAt")]
+    [BsonElement("modifiedAt"), Required, NotNull]
     public DateTime ModifiedAt { get; set; }
 
     /// <summary>
     /// User who saved the last modification of the document.
     /// </summary>
-    [BsonElement("modifiedBy")]
+    [BsonElement("modifiedBy"), Required, NotNull]
     public string ModifiedBy { get; set; } = null!;
 
     /// <summary>
     /// Number of times the document was changed - including the first add.
     /// </summary>
-    [BsonElement("version")]
+    [BsonElement("version"), Required, NotNull]
     public long ChangeCount { get; set; }
 }
 

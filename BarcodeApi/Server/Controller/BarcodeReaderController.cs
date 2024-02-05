57bacd11eb77f1e4911827149965daaefcf253ff
Dr. Jochen Manns <J.Manns@zera.de>
2024-02-05 08:38:21 +0100

    Helper to simulate barcodes in mock mode.

---
+++ BarcodeApi/Server/Controller/BarcodeReaderController.cs
@@ -0,0 +1,35 @@
+using BarcodeApi.Actions.Device;
+using BarcodeApi.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Swashbuckle.AspNetCore.Annotations;
+
+namespace ErrorCalculatorApi.Controllers;
+
+/// <summary>
+/// Helper to simulate barcode input.
+/// </summary>
+/// <param name="_device">Serial port connected device to use.</param>
+[ApiVersion("1.0")]
+[ApiController]
+[Route("api/v{version:apiVersion}/BarcodeReader/[controller]")]
+public class BarcodeReaderController(IBarcodeReader _device) : ControllerBase
+{
+    /// <summary>
+    /// Simulate a barcode.
+    /// </summary>
+    /// <param name="code">Barcode to use.</param>
+    /// <exception cref="NotImplementedException">Barcode reader is not in mock mode.</exception>
+    [HttpPut]
+    [SwaggerOperation(OperationId = "SimulateBarcode")]
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status400BadRequest)]
+    public Task<ActionResult> SimulateBarcode([FromBody] string code)
+    {
+        if (_device is not BarcodeReaderMock mock) throw new NotImplementedException("barcode reader is not a mock");
+
+        mock.Fire(code);
+
+        return Task.FromResult<ActionResult>(Ok());
+    }
+}

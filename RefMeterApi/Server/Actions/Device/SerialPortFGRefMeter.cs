bc8d4f7924ea52d323001ce57a69f33f354efca4
Dr. Jochen Manns <J.Manns@zera.de>
2024-03-08 11:54:06 +0100

    Meter constant readout for FG30x.

--- RefMeterApi/Server/Actions/Device/SerialPortFGRefMeter.cs
+++ RefMeterApi/Server/Actions/Device/SerialPortFGRefMeter.cs
@@ -1,3 +1,4 @@
+using System.Text.RegularExpressions;
 using Microsoft.Extensions.Logging;
 using RefMeterApi.Exceptions;
 using SerialPortProxy;
@@ -36,7 +37,31 @@ public partial class SerialPortFGRefMeter(ISerialPortConnection device, ILogger<
     public bool Available => true;
 
     /// <inheritdoc/>
-    public Task<double> GetMeterConstant() => throw new NotImplementedException();
+    public async Task<double> GetMeterConstant()
+    {
+        TestConfigured();
+
+        /* Request raw data from device. */
+        var biRequest = SerialPortRequest.Create("BI", new Regex(@"^BI(.+)$"));
+        var buRequest = SerialPortRequest.Create("BU", new Regex(@"^BU(.+)$"));
+
+        await Task.WhenAll(_device.Execute(biRequest, buRequest));
+
+        /* Convert text representations to numbers. */
+        var voltage = double.Parse(buRequest.EndMatch!.Groups[1].Value);
+        var current = double.Parse(biRequest.EndMatch!.Groups[1].Value);
+
+        var phases = SupportedModes.Single(e => e.Value == _measurementMode).Key[0] switch
+        {
+            '4' => 3d,
+            '3' => 2d,
+            '2' => 1d,
+            _ => throw new ArgumentException($"unsupported measurement mode {_measurementMode}")
+        };
+
+        /* Calculate according to formula - see MT78x_MAN_EXT_GB.pdf section 5.6.*/
+        return 1000d * 3600d * 60000d / (phases * (double)voltage * (double)current);
+    }
 
     /// <summary>
     /// See if the reference meter is configured.

b6956bc74e2f185b59122070d94cbd2aab786e46
Dr. Jochen Manns <J.Manns@zera.de>
2024-03-20 15:57:55 +0100

    Global error enumeration now in swagger.

--- Shared/Library/Models/SamGlobalErrors.cs
+++ Shared/Library/Models/SamGlobalErrors.cs
@@ -1,12 +1,15 @@
+using System.Text.Json.Serialization;
+
 namespace SharedLibrary.Models;
 
 /// <summary>
 /// Errors that can occur unpredictable
 /// </summary>
+[JsonConverter(typeof(JsonStringEnumConverter))]
 public enum SamGlobalErrors
 {
     /// <summary>
     /// 
     /// </summary>
-    GENERAL_ERROR
+    GENERAL_ERROR = 0
 }
--- Shared/Library/SharedLibraryExtensions.cs
+++ Shared/Library/SharedLibraryExtensions.cs
@@ -58,6 +58,7 @@ public static class SharedLibraryConfiguration
         SwaggerModelExtender
             .AddType<SamDetailExtensions>()
             .AddType<SamDatabaseError>()
+            .AddType<SamGlobalErrors>()
             .Register(options);
     }
 }

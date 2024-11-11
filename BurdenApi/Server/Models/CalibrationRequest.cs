4c3c9e74d90ce1f0bac119b93e06765b903f71f9
Dr. Jochen Manns <J.Manns@zera.de>
2024-11-11 09:53:01 +0100

    Added context information to burden calibration request. (zeragmbh/websam#782)

--- BurdenApi/Server/Models/CalibrationRequest.cs
+++ BurdenApi/Server/Models/CalibrationRequest.cs
@@ -9,6 +9,24 @@ namespace BurdenApi.Models;
 public class CalibrationRequest
 {
     /// <summary>
+    /// Burden to use.
+    /// </summary>
+    [NotNull, Required]
+    public string Burden { get; set; } = null!;
+
+    /// <summary>
+    /// Range to use.
+    /// </summary>
+    [NotNull, Required]
+    public string Range { get; set; } = null!;
+
+    /// <summary>
+    /// Step to use.
+    /// </summary>
+    [NotNull, Required]
+    public string Step { get; set; } = null!;
+
+    /// <summary>
     /// Target to find.
     /// </summary>
     [NotNull, Required]

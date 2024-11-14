a08d64cfe62c383e4df5ddb50d17b8dee668969d
Dr. Jochen Manns <J.Manns@zera.de>
2024-11-14 08:13:57 +0100

    Corrected ref meter status - ranges must be optional.

--- RefMeterApi/Server/Models/RefMeterStatus.cs
+++ RefMeterApi/Server/Models/RefMeterStatus.cs
@@ -5,22 +5,23 @@ namespace RefMeterApi.Models;
 /// <summary>
 /// 
 /// </summary>
-public class RefMeterStatus {
+public class RefMeterStatus
+{
     /// <summary>
     /// Actual input channel (e.g. UB=250 or K1=10V) 
     /// </summary>
-    public Voltage VoltageRange { get; set; }
+    public Voltage? VoltageRange { get; set; }
 
     /// <summary>
     /// Actual input channel (e.g. IB=10 or K2=0.02A(? -> I2=0.02A?))
     /// </summary>
-    public Current CurrentRange { get; set; }
+    public Current? CurrentRange { get; set; }
 
     /// <summary>
     /// 
     /// </summary>
     public MeasurementModes? MeasurementMode { get; set; }
-    
+
     /// <summary>
     /// PLL-Reference
     /// </summary>
